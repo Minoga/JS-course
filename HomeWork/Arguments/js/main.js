67868ee1f04146253e2cc28452582cd197c58397
Minoga <sergvoshod@gmail.com>
2013-04-29 23:06:30 +0400

    summa in array

--- HomeWork/Arguments/js/main.js
+++ HomeWork/Arguments/js/main.js
@@ -1,11 +1,13 @@
 function summaArray() {
+  var result = [];
   for (var i=0; i<arguments.length; i++) {
       var summa = 0;
       for (var j=0; j<arguments[i].length; j++) {
           summa = summa + arguments[i][j];
       }
-      console.log(summa)
+      result.push(summa)
   }
+  console.log(result)
 }
 
 summaArray([1,2,3,4,], [2,3,4,5,6], [2,3,4,5,16])
