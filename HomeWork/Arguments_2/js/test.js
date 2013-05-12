d4c9d3bac16c99f1fd256ffa95495c6e468ea340
Minoga <sergvoshod@gmail.com>
2013-05-12 23:16:52 +0400

    fixed test.js (variable for result and full comments)

--- HomeWork/Arguments_2/js/test.js
+++ HomeWork/Arguments_2/js/test.js
@@ -1,3 +1,4 @@
-console.assert(multiplyArrays([1, 2, 3, 4], [1, 2, 3, 4]).length == 2, "Error");
-console.assert(multiplyArrays([1, 2, 3, 4], [1, 2, 3, 4])[1] == 24, "Error");
-console.assert(multiplyArrays([1, 2, 3, 4], [1, 2, 3, 5])[2] == 24, "Error");
+var result = multiplyArrays([1, 2, 3, 4], [1, 2, 3, 4])
+console.assert(result.length == 2, "Error length array");
+console.assert(result[1] == 24, "Not right calculate elements");
+console.assert(multiplyArrays([1, 2, 3, 4], [1, 2, 3, 5])[2] == 24, "Not right calculate elements");
