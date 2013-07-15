5ed0016f492425d99ad85e64ae1e2a7a3e29f1b6
Minoga <sergvoshod@gmail.com>
2013-07-15 20:22:38 +0400

    Refactor

--- HomeWork/hw11/js/Button.js
+++ HomeWork/hw11/js/Button.js
@@ -1,11 +1,11 @@
 var Button = function(name, el) {
     this.name = name;
-    el.addEventListener('click', this.onclick.bind(this))
+    el.addEventListener('click', this.onclick.bind(this));
     this.func = this.log.bind(this);
 };
 Button.prototype.log = function() {
     console.log(this.name)
-}
+};
 Button.prototype.onclick = function() {
     this.log();
     setTimeout(this.func, 1000);
